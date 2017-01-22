3b1ba50f1cbd07123031cc0ba479d0613160c994
highiqkid <victor_cheng@yahoo.com>
2017-01-22 04:48:36 -0800

    change's

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.post('/webhook/', function (req, res) {
 		let sender = event.sender.id;
 		let state = 0;
 		console.log(sender);
-		pool.query('SELECT state FROM users WHERE message_id = $1' ,  [sender],  function (err, result) {
+		pool.query('SELECT state FROM users WHERE user_id = 1' ,  [sender],  function (err, result) {
 			//call `done()` to release the client back to the pool
 			console.log(result);
 			state = result.rows[0].state;
