3a1723a54fee57618d67611a35e57562497f9fee
highiqkid <victor_cheng@yahoo.com>
2017-01-21 22:35:25 -0800

    jo

--- esri_wrapper.js
+++ esri_wrapper.js
@@ -1,3 +1,22 @@
 /**
  * Created by Victor on 1/21/2017.
  */
+const ApiBuilder = require('claudia-api-builder');
+
+let logError = function (err) {
+    console.error(err);
+};
+
+module.exports = function botBuilder(messageHandler, options, optionalLogError) {
+    logError = optionalLogError || logError;
+
+    const api = new ApiBuilder(),
+        messageHandlerPromise = function (message, originalApiBuilderRequest) {
+            return Promise.resolve(message).then(message => messageHandler(message, originalApiBuilderRequest));
+        };
+
+    api.get('/', () => 'Ok');
+
+
+    return api;
+};
