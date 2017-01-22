2188ec2c5ad83bee8a0b6e4bb13c18bf475b094f
highiqkid <victor_cheng@yahoo.com>
2017-01-21 22:38:57 -0800

    test

--- index.js
+++ index.js
@@ -74,6 +74,7 @@ app.get('/webhook/', function (req, res) {
 
 // to post data
 app.post('/webhook/', function (req, res) {
+	console.log(req.body);
 	let messaging_events = req.body.entry[0].messaging;
 	for (let i = 0; i < messaging_events.length; i++) {
 		let event = req.body.entry[0].messaging[i]
@@ -90,7 +91,7 @@ app.post('/webhook/', function (req, res) {
 				//sendGenericMessage(sender)
 				continue
 			}
-			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+			sendTextMessage(sender, "Sorry not sure what you mean by " + text.substring(0, 200))
 		}
 		if (event.postback) {
 			let text = JSON.stringify(event.postback)
