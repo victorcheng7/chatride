69a1d0322faee7000b893c84088c402725c8f624
highiqkid <victor_cheng@yahoo.com>
2017-01-21 18:33:39 -0800

    change

--- index.js
+++ index.js
@@ -14,7 +14,40 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
-const pg = require('pg');
+var  pg = require('pg');
+var config = {
+	user: 'gxtazabgagnnhg', //env var: PGUSER
+	database: 'dct2b6f7sgpbd6', //env var: PGDATABASE
+	password: 'cbbd1bee740b2ea329657af09b2e2545c1bfb34a99132b650d7701337dfedde2', //env var: PGPASSWORD
+	host: 'ec2-54-235-177-45.compute-1.amazonaws.com', // Server hosting the postgres database
+	port: 5432, //env var: PGPORT
+	max: 10, // max number of clients in the pool
+	ssl: true
+//	idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+};
+var pool = new pg.Pool(config);
+
+pool.connect(function(err, client, done) {
+	if(err) {
+		return console.error('error fetching client from pool', err);
+	}
+	client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+		//call `done()` to release the client back to the pool
+		done();
+
+		if(err) {
+			return console.error('error running query', err);
+		}
+		console.log(result.rows[0].number);
+		//output: 1
+	});
+});
+
+pool.on('error', function (err, client) {
+	console.error('idle client error', err.message, err.stack)
+});
+
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -60,17 +93,6 @@ app.post('/webhook/', function (req, res) {
 		}
 	}
 	res.sendStatus(200)
-});
-
-
-const connectionString = process.env.DATABASE_URL;
-pg.connect(connectionString, function(err, client, done) {
-	console.log(err+"!!!!!!!!!!!!!!!");
-	client.query('SELECT * FROM users', function(err, result) {
-		done();
-		if(err) return console.error(err);
-		console.log(result.rows);
-	});
 });
 
 
